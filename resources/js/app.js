84ad932e6df338f8924df4ead185b74a5194fab1
daveheseling <daveheseling@gmail.com>
2018-09-16 22:05:21 +0200

    Add tailwind css framework

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('navigation-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
