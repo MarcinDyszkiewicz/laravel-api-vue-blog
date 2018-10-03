950becc933fb4f142ab2420c89ed43b647ba001a
daveheseling <daveheseling@gmail.com>
2018-10-03 20:14:52 +0200

    Add user controller helper

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,34 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\UserControllerHelper;
 use App\Models\User;
 use Illuminate\Http\Request;
 use phpseclib\Crypt\Hash;
 
 class UserController extends Controller
 {
+    use UserControllerHelper;
+
+    private $userControllerHelper;
+
+    public function __construct(UserControllerHelper $userControllerHelper)
+    {
+        $this->userControllerHelper = $userControllerHelper;
+    }
+
     /**
      * @param User $user
      * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(User $user, Request $request)
+    public function changePassword(Request $request, User $user)
     {
-        $this->validate($request, $this->rules(), $this->validationErrorMessages());
         $user->name = $request->name;
-        $user->password = Hash::make($request->new_password);
+        $user->password = bcrypt($request->new_password);
+        $user->save();
+
+        return response()->json(['message' => 'user has been updated', 'data'=>$user]);
     }
 
-    public function updateBySuperAdmin(User $user, Request $request)
-    {
-
-    }
-
-    protected function changeUserRole(User $user, Request $request)
+    public function changeRoleAndPermission(User $user, Request $request)
     {
         $user->role = $request->role;
         $user->save();
     }
-
     protected function rules()
     {
         return [
